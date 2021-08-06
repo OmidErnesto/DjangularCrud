4a9d94da58a656285eee12e261c6463a2d6daa9b
OmidErnesto <ochahuaris@unsa.edu.pe>
2021-08-06 04:27:04 -0500

    Mejorando el seralizador para obtener solo informacion necesaria

--- django/api/views.py
+++ django/api/views.py
@@ -1,9 +1,15 @@
+from rest_framework.response import Response
 from api.models import Movie
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from api.serializers import MovieSerializer
+from api.serializers import MovieMiniSerializer, MovieSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    def list(self, request, *args, **kwargs):
+        movies = Movie.objects.all()
+        serializer = MovieMiniSerializer(movies, many=True)
+        return Response(serializer.data)
