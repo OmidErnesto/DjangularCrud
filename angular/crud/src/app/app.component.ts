22bbea237726d6d4058230c0ca17617ec3c04899
OmidErnesto <ochahuaris@unsa.edu.pe>
2021-08-06 03:30:48 -0500

    Conectando Django con Angular y listando las dos peliculas

--- angular/crud/src/app/app.component.ts
+++ angular/crud/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  movies = [{title: 'titanic'},{title:'avatar'}];
+  movies = [{title: 'test'}];
+
+  constructor(private api:ApiService){
+    this.getMovies();
+  }
+  
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        this.movies = data;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+  
 }
--- django/djangocrud/settings.py
+++ django/djangocrud/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#CORS_ORIGIN_WHITELIST = [
+#    'https://localhost:4200'
+#]
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Application definition
 
@@ -38,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
